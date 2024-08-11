f5127371de945d820c65a72be7a148facef7ecfa
shirishtiwari01 <131908797+shirishtiwari01@users.noreply.github.com>
2024-08-11 10:02:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+image=Image.open(r'C:\Users\tiwar\OneDrive\Desktop\bank.jpeg')
+st.image(image,caption='Welcome to XYZ Bank')
+st.title('Loan Calculator')
+st.header('XYZ Bank of India')
+x=st.number_input('Enter your amount')
+y=st.number_input('Enter your salary')
+if y>50000:
+    st.write('Congratulations')
+    st.balloons()
+else:
+    st.write('Sorry')
+
+st.checkbox('Do you have a credit card')
+st.sidebar.header('Schemes from Loan dept. ')
