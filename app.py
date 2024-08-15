0513907756e27b088c8325bebe491f912cd206b6
shirishtiwari01 <131908797+shirishtiwari01@users.noreply.github.com>
2024-08-15 14:35:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-image=Image.open(r'C:\Users\tiwar\OneDrive\Desktop\bank.jpeg')
+image=Image.open('bank.jpeg')
 st.image(image,caption='Welcome to XYZ Bank')
 st.title('Loan Calculator')
 st.header('XYZ Bank of India')
